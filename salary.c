0134a93c70c9824d213bb9600080764cb9546b68
Ashutosh Dhawan <94750772+ashutoshdhawan2003@users.noreply.github.com>
2022-06-24 23:48:04 +0530

    Add files via upload

---
+++ salary.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int salary;
+float tax1,tax2,tax3;
+printf("Enter your salary\n");
+scanf("%d",&salary);
+tax1=salary*5/100;
+tax2=salary*20/100;
+tax3=salary*30/100;
+if(250000<salary<500000){
+    printf("you have to pay 5 percent of tax is %f",tax1);
+}
+else if(500000<salary<1000000){
+    printf("you have to pay 20 percent of tax is %f",tax2);
+}
+else if(salary>1000000){
+    printf("you have to pay 30 percent of tax is %f",tax3);
+}
+    return 0;
+}
